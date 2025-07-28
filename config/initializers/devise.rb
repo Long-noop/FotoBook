62a2490cd4dda39740756dcf83dc6e8f47cf6c9c
Long-noop <hoanglong434gl@gmail.com>
2025-07-28 13:40:01 +0700

    fix: rediredct uri oauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,7 +274,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
     scope: "userinfo.email, userinfo.profile",
-    redirect_uri: "http://127.0.0.1:3000/users/auth/google_oauth2/callback",
+    redirect_uri: ENV["GOOGLE_OAUTH_REDIRECT_URI"],
     prompt: "select_account"
 
   }
